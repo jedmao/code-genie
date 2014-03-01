9d5bb6701a1a0a2fe7b9c077ee7f08acbe710967
Jed Mao <jed.hunsaker@gmail.com>
2014-03-01 00:43:42 -0600

    Delete generated sinon-chai file

--- test/sinon-chai.js
+++
@@ -1,8 +0,0 @@
-﻿///<reference path='../bower_components/dt-node/node.d.ts'/>
-///<reference path='../bower_components/dt-mocha/mocha.d.ts'/>
-///<reference path='../bower_components/dt-sinon/sinon.d.ts'/>
-///<reference path='../bower_components/dt-sinon-chai/sinon-chai.d.ts'/>
-var chai = require('chai');
-var sinon = require('sinon');
-chai.use(require('sinon-chai'));
-exports.expect = chai.expect;
